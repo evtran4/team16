0d0640a3ac9996359d2dc7820b5ee2508cb04f26
Ezra Gashaw <ezra.t.gashaw@gmail.com>
2025-01-24 09:07:15 -0500

    added schemas

--- src/server/schemas.py
+++ src/server/schemas.py
@@ -3,3 +3,18 @@ from typing import List
 
 class TestSchema(BaseModel):
     test: str
+    
+class EmailSchema(BaseModel):
+    email: List[EmailStr]
+
+class AccountSchema(BaseModel):
+    email: str
+    password: str
+    name: str
+    major: str
+    tags: List[str]
+    likes: List[str]
+    seen: List[str]
+    dealbreakers: List[str]
+    bio: str
+    cookie: str
