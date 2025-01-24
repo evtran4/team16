46207ddae6c7f025fbe8a1b2020d24ed3a8ce6d5
Ezra Gashaw <ezra.t.gashaw@gmail.com>
2025-01-24 09:08:04 -0500

    added a couple serilizers

--- src/server/serializers.py
+++ src/server/serializers.py
@@ -3,3 +3,18 @@ def test_serializer(testdata) -> dict:
         "id": str(testdata["_id"]),
         "test": testdata["test"]
     }
+    
+def user_serializer(user) -> dict:
+    return {
+        "id": str(user["_id"]),
+        "name": user["name"],
+        "email": user["email"],
+        "password": user["password"],
+        "major": user["major"],
+        "tags" : user["tags"],
+        "likes": user["likes"],
+        "bio": user["bio"],
+        "seen": user["seen"],
+        "dealbreakers": user["dealbreakers"],
+        "cookie": user["cookie"]
+    }
